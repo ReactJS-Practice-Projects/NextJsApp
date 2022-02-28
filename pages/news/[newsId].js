f0fcc34f0632650068c55fc0757dd1831c896baf
andrey12v <andrey12v@gmail.com>
2022-02-27 19:41:20 -0600

    Add Link component to redirect to another page
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25616800#overview

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,3 @@
-//import the hook to get url parameter 
 import { useRouter } from 'next/router';
 
 // our-domain.com/news/something-important
@@ -6,14 +5,12 @@ import { useRouter } from 'next/router';
 function DetailPage() {
   const router = useRouter();
 
-  //here we use react router hook to get url parameter
   const newsId = router.query.newsId;
-  console.log(newsId);
 
   // send a request to the backend API
   // to fetch the news item with newsId
 
-  return <h1>The Detail Page</h1>
+  return <h1>The NewsId Detail Page</h1>
 }
 
 export default DetailPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,24 @@
 // our-domain.com/news
+//import Link component to use it for url references
+import Link from 'next/link';
+import { Fragment } from 'react';
 
+//when we use Link instead of <a></a> we don't have to go to another page and instead of it we stay on the same page 
+//Link just loads the requred component. It is bettre to use Link instead of anchor tags in NextJs applications
 function NewsPage() {
-  return <h1>The News Page</h1>
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/nextjs-is-a-great-framework'>
+            NextJS Is A Great Framework !
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
