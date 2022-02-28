8bd51e264a7f261ae73360fb52a2f5480a2f1f19
andrey12v <andrey12v@gmail.com>
2022-02-27 19:04:22 -0600

    Add pages with square brackets for dynamic path
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25616792#overview

---
+++ pages/news/[newsId].js
@@ -0,0 +1,11 @@
+// our-domain.com/news/something-important
+
+//if we want dynamic url we create file in square brackets 
+//so we can use any parameter, word in the url after news/ such as 'test' etc... 
+
+
+function DetailPage() {
+  return <h1>The Detail Page</h1>
+}
+
+export default DetailPage;
