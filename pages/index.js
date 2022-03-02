32d79bbedb04c687b543dce5fd5f9371a6ceb0eb
andrey12v <andrey12v@gmail.com>
2022-03-01 22:37:26 -0600

    Get better understanding about getStaticProps() and getServerSideProps()
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25616842#overview

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,32 @@ function HomePage(props) {
 }
 
 
+//getServerSideProps - the difference of this function with getStaticProps is that it will not run during the build process 
+//but it will away run after deployment on the server 
+//getServerSideProps - we get, prepare data for our components 
+
+// export async function getServerSideProps(context) {
+
+//with the context we get access to the request and response object 
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+
+//IF WE DON'T HAVE DATA THAT CHAGES ALL THE TIME AND WE DON'T NEED ACCESS TO TEH REQUEST OBJECT (EX. FOR AUTHENTICATION) THEN  getStaticProps IS BETTER
+//getStaticProps is faster by fatching the data for every incoming request because it can be cached and reused instead of regenerated all the time 
+
+//IF OUR DATA CHANGES ALL THE TIME THEN WE NEED TO USE getServerSideProps
+
+
 //getStaticProps - is reserved word
 //getStaticProps calls static props such as DUMMY_MEETUPS before it calls component functions 
 //the job of getStaticProps to prepare props for componet functions
@@ -35,7 +61,9 @@ export async function getStaticProps() {
   return {
     props: {
       meetups: DUMMY_MEETUPS
-    }
+    },
+    //revalidate - is the parameter that specifies how often the page should be updated on the server after deployment
+    revalidate: 1
   }; 
 }
 
