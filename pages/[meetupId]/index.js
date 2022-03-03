2f36442b900bd58cc2823d8b2ffe006c747b49a5
andrey12v <andrey12v@gmail.com>
2022-03-02 22:50:20 -0600

    Implement getStaticProps and getStaticPaths on page with dynamic parameters
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25616848#overview

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,46 @@ function MeetupDetails() {
   );
 }
 
+//here we use getStaticPaths - to get parameters 
+export async function getStaticPaths() {
+  return {
+    //fallback = false - indicates that we support only defined parameters here 
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  //here we get url parameters form context
+  const meetupId = context.params.meetupId;
+
+  console.log('meetup_id' + meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
